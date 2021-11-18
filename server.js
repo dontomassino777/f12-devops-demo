588d2c86fd67c9894abe81e54e7a695268d51888
Tomas Fisher <tomasfisher@Tomass-MBP.localdomain>
2021-11-18 11:29:53 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+const students = ["Tommy"];
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/api/students", (req, res) => {
+    res.status(200).send(students);
+});
+
+const port = process.env.port || 4545;
+
+app.listen(port, () => {
+    console.log(`Jammin on ${port}`);
+});
+
